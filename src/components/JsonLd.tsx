6df9607a6535de77f7bb302c1befd97c8ee0e623
anastasiyaklyap <anastasiya.klyap@gmail.com>
2026-08-06 13:02:31 +0200

    fix empty static HTML on subpages + structured data

--- src/components/JsonLd.tsx
+++ src/components/JsonLd.tsx
@@ -5,7 +5,6 @@ type JsonLdProps = {
 export const JsonLd = ({ data }: JsonLdProps) => (
 	<script
 		type="application/ld+json"
-		// biome-ignore lint/security/noDangerouslySetInnerHtml: <explanation>
 		dangerouslySetInnerHTML={{
 			__html: JSON.stringify(data).replace(/</g, "\\u003c"),
 		}}
